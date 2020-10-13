cb8ef57cca655a2d7ea199b2ec0d9a256a6d2c68
GadoliniumPotassium <gdkh514@gmail.com>
2020-10-12 20:45:05 -0400

    improved upon smartlights class

--- src/Model/SmartLight.java
+++ src/Model/SmartLight.java
@@ -4,13 +4,32 @@ public class SmartLight extends SmartModule {
 
     private final boolean isDimmable;
     private int lightPercentage;
+    private boolean isOn;
 
-    public SmartLight(boolean isDimmable, int lightPercentage,location where) {
-        super(where);
+    public SmartLight(location whereIsModule, boolean isDimmable, int lightPercentage, boolean isOn) {
+        super(whereIsModule);
         this.isDimmable = isDimmable;
         this.lightPercentage = lightPercentage;
-
+        this.isOn = isOn;
     }
 
+    public boolean isDimmable() {
+        return isDimmable;
+    }
 
+    public int getLightPercentage() {
+        return lightPercentage;
+    }
+
+    public void setLightPercentage(int lightPercentage) {
+        this.lightPercentage = lightPercentage;
+    }
+
+    public boolean isOn() {
+        return isOn;
+    }
+
+    public void setOn(boolean on) {
+        isOn = on;
+    }
 }
