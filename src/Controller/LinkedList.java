2ef5a312eaeaee8974b48f6492abc7f3310567ff
GadoliniumPotassium <gdkh514@gmail.com>
2020-10-13 17:28:33 -0400

    fixing a problem

--- src/Controller/LinkedList.java
+++ src/Controller/LinkedList.java
@@ -99,10 +99,18 @@ public class LinkedList {
      * @param module
      * @return
      */
+<<<<<<< HEAD
     public boolean removeModule(SmartModule module) {
         if (head.module == module) {
             head = head.next;
             head.prev = null;
+=======
+    public boolean removeModule(SmartModule module){
+        if (head.module==module){
+            head=head.next;
+            head.prev=null;
+            return true;
+>>>>>>> 0c48797199c80fdd2d5a9e3e95c8c5858abe0c16
         }
         whereAmI = head;
         while (whereAmI.next.module != module) {
