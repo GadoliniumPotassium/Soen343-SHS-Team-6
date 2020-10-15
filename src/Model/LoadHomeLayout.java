5631029141e8eed3e661ef542548cf7704c08867
rex884 <mikeyrex884@gmail.com>
2020-10-15 12:50:19 -0400

    D:\SOEN 343 Project\Soen343-SHS\src\Model\LoadHomeLayout.java

--- src/Model/LoadHomeLayout.java
+++ src/Model/LoadHomeLayout.java
@@ -1,4 +1,22 @@
 package Model;
 
 public class LoadHomeLayout {
+	public String[][] readFile(String filename){
+        BufferedReader br=new BufferedReader(new FileReader(filename));
+        ArrayList <String> lines=new ArrayList<String>();
+        String line=null;
+        while((line=br.readLine())!=null){
+        lines.add(line);
+        }
+        br.close();
+        String information [][]=new String[lines.size()][5];
+        for (int i=0;i<information.length;i++){
+            String y=lines.get(i);
+            String z []=y.split(";");
+            for(int j=0;j<information[i].length;j++){
+                information[i][j]=z[j];
+            }
+        }
+        return information;
+    }
 }
